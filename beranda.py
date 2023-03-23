82dfaed5df3c66c60acda9da1e2c10fcef2f525f
Salman Alfarisi <95122631+salmanalfarisi29@users.noreply.github.com>
2023-03-23 14:42:22 +0700

    FEAT  : BERANDA

--- beranda.py
+++ beranda.py
@@ -10,10 +10,14 @@ def main_page():
     with st.container():
         col1, col2 = st.columns([9,3])
         with col1:
-            st.write('MUHAMAD FARID AKBAR', '| SALMAN ALFARISI')
+            st.title('KELOMPOK MADEV 2.0')
+            st.write('BAGUS NUGROHO | 211511034')
+            st.write('MUHAMMAD RIVAN RIVALDI | 211511048')
+            st.write('REYNA NUR RAHMAH SETIANA | 211511054')
+            st.write('SALMAN ALFARISI | 211511059')
             st.write('PROGRAM STUDI', 'D3 TEKNIK INFORMATIKA')
             st.write('KELAS', '2B')
-        with col2:
-            st.image(img)
+        # with col2:
+        #     st.image(img)
 if __name__ == '__main__':
 	main_page()
